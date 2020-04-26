2077ff191109afdd29694b284b918ef873b6de7b
Michel <Michel@Luz>
2020-04-25 23:22:31 -0300

    Nuevo intento con netbeans nuevo

--- src/rompiendoexcel/RompiendoExcel.java
+++ src/rompiendoexcel/RompiendoExcel.java
@@ -16,7 +16,7 @@ public class RompiendoExcel {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        println("hola")
+        System.out.println("hola");
     }
     
 }

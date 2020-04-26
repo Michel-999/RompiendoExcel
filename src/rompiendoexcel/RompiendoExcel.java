7625c8babfa09d1301bfccf95ef5d847735f1a80
Michel <Michel@Luz>
2020-04-25 21:53:01 -0300

    Tercer Commit Espero que sea la vencida

--- src/rompiendoexcel/RompiendoExcel.java
+++ src/rompiendoexcel/RompiendoExcel.java
@@ -5,63 +5,17 @@
  */
 package rompiendoexcel;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 /**
  *
  * @author Michel
  */
 public class RompiendoExcel {
 
-public class Ejemplo {
-    
-    /** Creates a new instance of PruebaRuntime */
-    public Ejemplo() 
-    {
-        try
-        {
-            // Se lanza el ejecutable.
-            Process p=Runtime.getRuntime().exec ("cmd /c dir");
-            
-            // Se obtiene el stream de salida del programa
-            InputStream is = p.getInputStream();
-            
-            /* Se prepara un bufferedReader para poder leer la salida más comodamente. */
-            BufferedReader br = new BufferedReader (new InputStreamReader (is));
-            
-            // Se lee la primera linea
-            String aux = br.readLine();
-            
-            // Mientras se haya leido alguna linea
-            while (aux!=null)
-            {
-                // Se escribe la linea en pantalla
-                System.out.println (aux);
-                
-                // y se lee la siguiente.
-                aux = br.readLine();
-            }
-        } 
-        catch (Exception e)
-        {
-            // Excepciones si hay algún problema al arrancar el ejecutable o al leer su salida.*/
-            e.printStackTrace();
-        }
-    }
-    
     /**
-     * Crea la clase principal que ejecuta el comando dir y escribe en pantalla
-     * lo que devuelve dicho comando.
-     *
      * @param args the command line arguments
      */
-    public void main(String[] args) {
-        
-        Ejemplo ej;
-        
-        ej = new Ejemplo();
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
-} 
+    
 }
